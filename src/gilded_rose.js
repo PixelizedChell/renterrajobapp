6759a3a4d5967166118219bb7ce9aa75bccfc640
Chelsea Lipson <chelseamlipson@gmail.com>
2022-10-02 21:31:29 -0700

    Added tests and refactored logic for the backstage pass.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,34 +13,40 @@ class Shop {
   updateQuality() {
     //loop through items
     for (let i = 0; i < this.items.length; i++) {
-      //if items aren't special items
-      if (
-        (this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") &&
-        this.items[i].name != "Sulfuras, Hand of Ragnaros"
+      //if items aren't brie or concert tickets
+      if (this.items[i].name != "Aged Brie" &&
+        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
         //if quality > 0 decrease by 1
         if (this.items[i].quality > 0) {
-          this.items[i].quality -= 1;
+          //exclude Sulfuras so that it's not in the else block but is still excluded
+          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+            this.items[i].quality -= 1;
+          }
         }
+      //if they are brie or concert tickets
       } else {
         //if the quality is less than 50
         if (this.items[i].quality < 50) {
-          if (this.items[i].name === "Aged Brie") {
-            this.items[i].quality += 1;
-          }
+          this.items[i].quality = this.items[i].quality + 1;
           if (
             this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
           ) {
+            //if it is 10 days or less to the concert, increase quality
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality += 1;
               }
             }
+            //if it is 5 days or less to the concert, increase quality again
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality += 1;
               }
+            }
+            //drop quality to 0 after the concert
+            if (this.items[i].sellIn == 0) {
+              this.items[i].quality = 0;
             }
           }
         }
--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,4 +1,6 @@
 const {Shop, Item} = require("../src/gilded_rose");
+const pass = "Backstage passes to a TAFKAL80ETC concert";
+const sulfuras = "Sulfuras, Hand of Ragnaros";
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
@@ -15,5 +17,21 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("Aged Brie", 5, 20)])
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(21)
+  })
+
+  it("should increase in quality by two if the item is a backstage pass and it is less than 10 days to the concert", function() {
+    const gildedRose = new Shop([new Item(pass, 9, 20)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(22)
+  })
+  it("should increase in quality by three if the item is a backstage pass and it is less than 5 days to the concert", function() {
+    const gildedRose = new Shop([new Item(pass, 4, 20)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(23)
+  })
+  it("should increase in quality by three if the item is a backstage pass and the concert has passed", function() {
+    const gildedRose = new Shop([new Item(pass, 0, 20)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0)
   })
 });
