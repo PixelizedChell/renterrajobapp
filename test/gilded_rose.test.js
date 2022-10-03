c575c54150d6e861b6b5d38e53dcea89a66a5b93
Chelsea Lipson <chelseamlipson@gmail.com>
2022-10-02 20:36:48 -0700

    Created basic test to show quality degrading

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -4,6 +4,11 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
+  it("should degrade in quality after one day", function() {
+    const gildedRose = new Shop([new Item("apple", 5, 20)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(19)
+  })
 });
