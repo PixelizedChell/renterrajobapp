1d3e1bc3347e4d5a2e59cf5ac24e5ad0fa5849e0
Chelsea Lipson <chelseamlipson@gmail.com>
2022-10-02 20:48:09 -0700

    Began refactor and added test and logic for Aged Brie.

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -7,8 +7,13 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toBe("foo");
   });
   it("should degrade in quality after one day", function() {
-    const gildedRose = new Shop([new Item("apple", 5, 20)])
+    const gildedRose = new Shop([new Item("Apple", 5, 20)])
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(19)
+  })
+  it("should increase in quality after one day if it is Aged Brie", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 5, 20)])
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(21)
   })
 });
